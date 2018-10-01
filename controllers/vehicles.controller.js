2d60dcb87ce28ba43ef93a9f1699e34a8f6aadc8
Paula Quitián <pqhernandez@gmail.com>
2018-10-02 00:20:42 +0200

    feat: add vehicle to owner's vehicles array

--- controllers/vehicles.controller.js
+++ controllers/vehicles.controller.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Vehicle = require('../models/vehicle.model');
+const User = require('../models/user.model');
 
 module.exports.create = (req, res, next) => {
     Vehicle.findOne({ licensePlate: req.body.licensePlate })
@@ -8,9 +9,15 @@ module.exports.create = (req, res, next) => {
                 vehicle = new Vehicle(req.body);
                 return vehicle.save()
                     .then(vehicle => {
-                        //TODO: add vehicle.id to this user(req.user)
-                        
                         res.status(201).json(vehicle)
+                        return User.findByIdAndUpdate({ _id: req.user.id })
+                            .then(user => {
+                                if (user) {
+                                    user.vehicles.push(vehicle.id) //ASK QUESTION: is this id or _id?
+                                } else {
+                                    throw createError(404, 'User not found');
+                                }
+                            })
                     })
             } else {
                 throw createError(409, `Vehicle with ${req.body.licensePlate} is already registered`);
